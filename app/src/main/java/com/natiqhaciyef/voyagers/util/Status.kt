dd0ce2bf543deab271f10a107d1fb9b27c47d5a8
Natiq Haciyev <100693634+natiqhaciyef@users.noreply.github.com>
2023-03-21 14:43:19 +0400

    Initial commit

---
+++ app/src/main/java/com/natiqhaciyef/voyagers/util/Status.kt
@@ -0,0 +1,7 @@
+package com.natiqhaciyef.interviewsland_mobile_android.util
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
---
+++ app/src/main/java/com/natiqhaciyef/voyagers/view/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.natiqhaciyef.voyagers.view
+
+import androidx.compose.ui.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.natiqhaciyef.voyagers.view.ui.theme.*
+import androidx.compose.ui.tooling.preview.Preview
+import com.natiqhaciyef.voyagers.view.ui.theme.VoyagersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit){
+    VoyagersTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppTheme {
+
+    }
+}
+
+@get: Composable
+val Colors.myExtraColor: Color
+    get() = if (isLight) Red else Green
