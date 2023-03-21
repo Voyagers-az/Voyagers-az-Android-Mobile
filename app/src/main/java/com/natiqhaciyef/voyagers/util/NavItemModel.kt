795e2c1f9e8b4776e3c653556054533b57e108ec
Natiq Haciyev <100693634+natiqhaciyef@users.noreply.github.com>
2023-03-21 23:44:18 +0400

    UI - Splash and Register screen created

---
+++ app/src/main/java/com/natiqhaciyef/voyagers/util/NavItemModel.kt
@@ -0,0 +1,6 @@
+package com.natiqhaciyef.voyagers.util
+
+data class NavItemModel(
+    val image: Int,
+    val title: String
+)
--- app/src/main/java/com/natiqhaciyef/voyagers/view/navigation/AppNavigation.kt
+++ app/src/main/java/com/natiqhaciyef/voyagers/view/navigation/AppNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.natiqhaciyef.voyagers.view.screens.RegisterScreen
 import com.natiqhaciyef.voyagers.view.screens.SplashScreen
 
 
@@ -12,11 +13,11 @@ fun AppNavigation(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = ScreenID.SplashScreen.name){
         composable(route = ScreenID.SplashScreen.name){
-            SplashScreen()
+            SplashScreen(navController)
         }
 
         composable(route = ScreenID.RegisterScreen.name){
-
+            RegisterScreen(navController)
         }
 
         composable(route = ScreenID.LoginScreen.name){
