5cb5867e5cb19b4f4b9d6b4890e37784a31952b3
Natiq Haciyev <100693634+natiqhaciyef@users.noreply.github.com>
2023-04-13 15:38:54 +0400

    UI - Camera permission config

--- app/src/main/java/com/natiqhaciyef/voyagers/view/screens/home/PersonalInformationScreen.kt
+++ app/src/main/java/com/natiqhaciyef/voyagers/view/screens/home/PersonalInformationScreen.kt
@@ -1,15 +1,25 @@
 package com.natiqhaciyef.voyagers.view.screens.home
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.natiqhaciyef.voyagers.view.ui.theme.AppWhiteLightPurple
 
 @Preview
 @Composable
-fun PersonalInformationScreen(){
-    Column(modifier = Modifier.fillMaxSize()) {
+fun PersonalInformationScreen() {
 
-    }
 }
